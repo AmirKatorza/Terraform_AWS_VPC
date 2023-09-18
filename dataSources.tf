8e0cf244e7810d02aafa77dc48dc157e328931f4
Amir Katorza <amir.katorza@gmail.com>
2023-09-18 16:16:40 +0300

    Final Version for task1

--- dataSources.tf
+++ dataSources.tf
@@ -7,13 +7,3 @@ data "aws_ami" "server_ami_debian" {
     values = ["debian-12-amd64-*-1438"]
   }
 }
-
-# data "aws_ami" "server_ami_ubuntu" {
-#   most_recent = true
-#   owners      = ["099720109477"]
-
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-#   }
-# }
